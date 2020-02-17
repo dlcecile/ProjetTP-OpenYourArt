817ac53bcc995311562edf912fc7e52f8b9c1bc2
dlcecile <deleucecile@outlook.fr>
2020-02-17 12:34:52 +0100

    modification card jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
 	});
 });
 // ====================================
+$(document).ready(function(){
+  $(".card").mouseover(function(){
+    $(".btn").show();
+  });
+  $(".card").mouseout(function(){
+    $(".btn").hide();
+		  });
+	$(".btn").mouseover(function(){
+		$(".btn").css({"background": "#BB0408", "color": "white"});
+	});
+	$(".btn").mouseout(function(){
+		$(".btn").css({"background": " linear-gradient( #c3aa5b, white)", "color": "black"});
+	});
+});
